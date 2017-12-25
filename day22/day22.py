679162195b4498fcf9447fe914f5d1a53820f708
Matt Poepping <mattpoepping@Matts-MacBook-Pro-2.local>
2017-12-24 20:05:41 -0700

    day 22 done

---
+++ day22/day22.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+import numpy as np
+
+# N = 0
+# E = 1
+# S = 2
+# W = 3
+# xmove,ymove
+move = [ (0,-1), (1,0), (0,1) , (-1,0)]
+curDir = 0
+infectCount = 0
+
+inp = [1 if x == '#' else 0 for x in list("".join(list(map(lambda x: x.strip(), open("input2.txt").readlines()))))]
+
+inp = np.array(inp)
+inp = np.resize(inp, (25,25))
+inp = np.pad(inp, 10000, 'constant')
+
+# find the center
+x = y = cp = int(np.floor(len(inp[0])/2))
+
+for i in range(10000):
+	# if on clean node turn left
+	if inp[y][x] == 0:
+		curDir -= 1
+		if curDir < 0:
+			curDir = 3
+		inp[y][x] = 1
+		infectCount +=1
+		x = x + move[curDir][0]
+		y = y + move[curDir][1]
+	# on an infected node, turn right
+	else:
+		curDir += 1
+		if curDir > 3:
+			curDir = 0
+		inp[y][x] = 0
+		x = x + move[curDir][0]
+		y = y + move[curDir][1]
+
+	#print(inp)
+
+print(inp)
+print(infectCount)
